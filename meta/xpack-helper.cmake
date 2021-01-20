ee2f2a80c87bc9e476118cbac51806f1435da4c5
Liviu Ionescu <ilg@livius.net>
2021-01-20 22:29:25 +0200

    add meta/xpack-helper.cmake

---
+++ meta/xpack-helper.cmake
@@ -0,0 +1,38 @@
+#
+# This file is part of the µOS++ distribution.
+#   (https://github.com/micro-os-plus)
+# Copyright (c) 2021 Liviu Ionescu
+#
+# This Source Code Form is subject to the terms of the MIT License.
+# If a copy of the license was not distributed with this file, it can
+# be obtained from https://opensource.org/licenses/MIT/.
+#
+# -----------------------------------------------------------------------------
+
+message(STATUS "Including micro-os-plus-startup...")
+
+function(target_sources_micro_os_plus_startup target)
+
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+
+  target_sources(
+    ${target}
+
+    PRIVATE
+      ${PARENT_DIR}/src/startup.cpp
+  )
+endfunction()
+
+function(target_include_directories_micro_os_plus_startup target)
+
+  get_filename_component(PARENT_DIR ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+
+  target_include_directories(
+    ${target}
+
+    PUBLIC
+      ${PARENT_DIR}/include
+  )
+endfunction()
+
+# -----------------------------------------------------------------------------
