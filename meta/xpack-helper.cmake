9b3ded840b933c6cfa623ea69e27753a9a1be79f
Liviu Ionescu <ilg@livius.net>
2021-02-25 15:45:11 +0200

    cmake: simplify, remove functions

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -18,57 +18,53 @@ set(micro-os-plus-startup-included TRUE)
 message(STATUS "Including micro-os-plus-startup...")
 
 # -----------------------------------------------------------------------------
+# Dependencies.
 
-function(add_libraries_micro_os_plus_startup)
+find_package(micro-os-plus-version REQUIRED)
+find_package(micro-os-plus-diag-trace REQUIRED)
 
-  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+# -----------------------------------------------------------------------------
+# The current folder.
 
-  # ---------------------------------------------------------------------------
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
-  find_package(micro-os-plus-version)
-  find_package(micro-os-plus-diag-trace)
+# -----------------------------------------------------------------------------
 
-  # ---------------------------------------------------------------------------
+if (NOT TARGET micro-os-plus-startup-static)
 
-  if (NOT TARGET micro-os-plus-startup-static)
-
-    add_library(micro-os-plus-startup-static STATIC EXCLUDE_FROM_ALL)
-
-    # -------------------------------------------------------------------------
-
-    target_sources(
-      micro-os-plus-startup-static
-  
-      PRIVATE
-        ${xpack_current_folder}/src/startup.cpp
-    )
-
-    target_include_directories(
-      micro-os-plus-startup-static
-  
-      PUBLIC
-        ${xpack_current_folder}/include
-    )
-
-    target_link_libraries(
-      micro-os-plus-startup-static
-      
-      PUBLIC
-        micro-os-plus::version
-        micro-os-plus::diag-trace-static
-        micro-os-plus::architecture
-    )
-
-    # -------------------------------------------------------------------------
-    # Aliases
-
-    add_library(micro-os-plus::startup-static ALIAS micro-os-plus-startup-static)
-    message(STATUS "micro-os-plus::startup-static")
-
-  endif()
+  add_library(micro-os-plus-startup-static STATIC EXCLUDE_FROM_ALL)
 
   # ---------------------------------------------------------------------------
 
-endfunction()
+  target_sources(
+    micro-os-plus-startup-static
+
+    PRIVATE
+      ${xpack_current_folder}/src/startup.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-startup-static
+
+    PUBLIC
+      ${xpack_current_folder}/include
+  )
+
+  target_link_libraries(
+    micro-os-plus-startup-static
+    
+    PUBLIC
+      micro-os-plus::version
+      micro-os-plus::diag-trace-static
+      micro-os-plus::architecture
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::startup-static ALIAS micro-os-plus-startup-static)
+  message(STATUS "micro-os-plus::startup-static")
+
+endif()
 
 # -----------------------------------------------------------------------------
