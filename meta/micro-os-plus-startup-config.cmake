e780d4fad26e58a7123d435bb2a7185f60a1405a
Liviu Ionescu <ilg@livius.net>
2021-02-25 16:46:23 +0200

    cmake: simplify, keep only config.cmake

--- meta/micro-os-plus-startup-config.cmake
+++ meta/micro-os-plus-startup-config.cmake
@@ -12,11 +12,62 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including micro-os-plus-startup-config...")
+if(micro-os-plus-startup-included)
+  return()
+endif()
+
+set(micro-os-plus-startup-included TRUE)
+
+message(STATUS "Including micro-os-plus-startup...")
+
+# -----------------------------------------------------------------------------
+# Dependencies.
+
+find_package(micro-os-plus-version REQUIRED)
+find_package(micro-os-plus-diag-trace REQUIRED)
+
+# -----------------------------------------------------------------------------
+# The current folder.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+if (NOT TARGET micro-os-plus-startup-static)
+
+  add_library(micro-os-plus-startup-static STATIC EXCLUDE_FROM_ALL)
+
+  # ---------------------------------------------------------------------------
+
+  target_sources(
+    micro-os-plus-startup-static
+
+    PRIVATE
+      ${xpack_current_folder}/src/startup.cpp
+  )
+
+  target_include_directories(
+    micro-os-plus-startup-static
+
+    PUBLIC
+      ${xpack_current_folder}/include
+  )
+
+  target_link_libraries(
+    micro-os-plus-startup-static
+    
+    PUBLIC
+      micro-os-plus::version
+      micro-os-plus::diag-trace-static
+      micro-os-plus::architecture
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(micro-os-plus::startup-static ALIAS micro-os-plus-startup-static)
+  message(STATUS "micro-os-plus::startup-static")
+
+endif()
 
 # -----------------------------------------------------------------------------
-
