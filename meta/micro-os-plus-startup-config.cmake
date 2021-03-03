24c9d1cb6e5afd57d43370cbf43de7c5cf9ccb0f
Liviu Ionescu <ilg@livius.net>
2021-03-04 00:36:13 +0200

    cmake: add xpack_process_package_version()

--- meta/micro-os-plus-startup-config.cmake
+++ meta/micro-os-plus-startup-config.cmake
@@ -18,7 +18,7 @@ endif()
 
 set(micro-os-plus-startup-included TRUE)
 
-message(STATUS "Including micro-os-plus-startup...")
+message(STATUS "Processing xPack ${PACKAGE_JSON_NAME}@${PACKAGE_JSON_VERSION}...")
 
 # -----------------------------------------------------------------------------
 # Dependencies.
