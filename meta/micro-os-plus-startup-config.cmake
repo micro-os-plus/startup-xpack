cd166578fcf09ccd553762940baec6a8f556fd86
Liviu Ionescu <ilg@livius.net>
2021-03-04 10:20:47 +0200

    cmake: use xpack_glob_recurse_cxx()

--- meta/micro-os-plus-startup-config.cmake
+++ meta/micro-os-plus-startup-config.cmake
@@ -39,7 +39,8 @@ if (NOT TARGET micro-os-plus-startup-static)
 
   # ---------------------------------------------------------------------------
 
-  file(GLOB_RECURSE source_files CONFIGURE_DEPENDS "${xpack_current_folder}/src/*.c*")
+  xpack_glob_recurse_cxx(source_files "${xpack_current_folder}/src")
+  xpack_display_relative_paths("${source_files}" "${xpack_current_folder}")
 
   target_sources(
     micro-os-plus-startup-static
