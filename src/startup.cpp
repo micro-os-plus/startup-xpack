fce1977a50ab6c6046be8486cf5e273d41f0b0e0
Liviu Ionescu <ilg@livius.net>
2026-06-11 22:40:15 +0300

    prefer project-config.h

--- src/startup.cpp
+++ src/startup.cpp
@@ -14,9 +14,12 @@
 
 // ----------------------------------------------------------------------------
 
-#if __has_include(<micro-os-plus/config.h>)
+#if __has_include(<micro-os-plus/project-config.h>)
+#include <micro-os-plus/project-config.h>
+#elif __has_include(<micro-os-plus/config.h>)
+#pragma message "micro-os-plus/config.h is deprecated, rename to micro-os-plus/project-config.h and include it instead of micro-os-plus/config.h"
 #include <micro-os-plus/config.h>
-#endif // __has_include(<micro-os-plus/config.h>)
+#endif // __has_include(<micro-os-plus/project-config.h>)
 
 #if defined(MICRO_OS_PLUS_INCLUDE_STARTUP)
 
